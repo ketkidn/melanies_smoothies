0b1b1eb0de677799e5bc4b63be4a7949b314de76
ketkidn <47437361+ketkidn@users.noreply.github.com>
2025-12-01 14:58:54 +0100

    Fix indentation for ingredients processing

--- streamline_app.py
+++ streamline_app.py
@@ -33,9 +33,9 @@ if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
 
 # Insert order
 if st.button("Submit Order") and in_list:
