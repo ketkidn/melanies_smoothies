fd0d6781ee2c115b3630def47c2e448b0dd3f225
ketkidn <47437361+ketkidn@users.noreply.github.com>
2025-12-01 09:41:56 +0100

    Refactor Snowflake session handling in app

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -20,7 +20,8 @@ name_order=st.text_input('Name on Smoothie:')
 ##st.write("You selected:", option)
 
 
-session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 in_list=st.multiselect('Choose up to 5 ingredients:'
